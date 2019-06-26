a0a87a2cb38168a341b035e217165e8fe2dfee67
ISTLOJA1 <ISTLOJA1@ISTLOJA17>
2019-06-26 09:17:25 -0500

    Deber

---
+++ PracticaPooLuisfernando/src/Deber/NewClass4.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Deber;
+
+/**
+ *
+ * @author ISTLOJA1
+ */
+public class NewClass4 {
+    private String cadena;
+ 
+
+ 
+public NewClass4(String mensaje)
+{  
+cadena = mensaje;
+} 
+ 
+ 
+ 
+public void Presenta()
+{
+ 
+int contador = 1;
+System.out.print("\n 1 10*N 100*N 1000*N\n\n");
+ 
+while ( 5 >= contador)
+{ 
+System.out.printf("%6d %6d %6d %6d\n", contador, 10*contador, 100*contador, 1000*contador++);
+} 
+ 
+} 
+} 
+
+ 
+
---
+++ PracticaPooLuisfernando/src/Deber/NewClass7.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Deber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ISTLOJA1
+ */
+public class NewClass7 {
+     public void procesarResultadosExamen()
+{ 
+
+Scanner entrada = new Scanner(System.in);
+int aprobados = 0;
+int reprobados = 0;
+int contadorEstudiantes = 1;
+int resultado;
+
+while (contadorEstudiantes <= 10)
+{ 
+System.out.println("\nEscriba el resultado (1 = aprobado, 2 = reprobado)");
+resultado = entrada.nextInt();
+
+if ( 1 == resultado)
+aprobados = aprobados + 1;
+else
+reprobados = reprobados + 1;
+
+contadorEstudiantes = contadorEstudiantes + 1;
+} 
+System.out.printf("\nAprobados: %d\nReprobados: %d\n", aprobados, reprobados);
+
+if (aprobados > 8)
+System.out.println("\nAumentar colegiatura.");
+}
+}
+
